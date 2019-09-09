d6307f5f4f7c1ddf0dbdc863a6573ee32c0988b2
Gideon <g.vdherik@live.nl>
2019-09-09 14:38:57 +0200

    LAB 1
    Done

--- LABS/LAB 1/js/b1w2l1.js
+++ LABS/LAB 1/js/b1w2l1.js
@@ -1,1 +1,9 @@
 // begin hier je JavaScript commandos
+
+var firstname = prompt("Wat is uw voornaam?");
+var lastname = prompt("Wat is uw achternaam?");
+var age = prompt("Wat is uw leeftijd?");
+
+document.writeln("Uw voornaam is: " + firstname + "<br>");
+document.writeln("Uw achternaam is: " + lastname + "<br>");
+document.writeln("Uw leeftijd is: " + age + "<br>");
